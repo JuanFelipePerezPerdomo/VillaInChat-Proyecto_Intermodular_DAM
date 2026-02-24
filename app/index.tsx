66c6b603f416845813e27ea4130457a7fce33fd0
JuanFelipePerezPerdomo <juanfelipeperezperdomo@gmail.com>
2026-02-24 16:01:50 +0000

    pantallas y componentes templates

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,28 @@
-import { Text, View } from "react-native";
+import {
+  Button, Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle
+} from "@/src/components/ui";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <Empty>
+      <EmptyHeader>
+      <EmptyMedia variant="icon">
+        <Ionicons name="chatbubble-outline" size={50}></Ionicons>
+      </EmptyMedia>
+      <EmptyTitle> No hay ningun chat creado </EmptyTitle>
+      <EmptyDescription> Cree un nuevo chat</EmptyDescription>
+      </EmptyHeader>
 
-    </View>
+      <EmptyContent>
+        <Button
+          title="Crear un nuevo chat"
+          onPress={() => <Link href="/"></Link>}
+        />
+      </EmptyContent>
+    </Empty>
+
+    
   );
 }
